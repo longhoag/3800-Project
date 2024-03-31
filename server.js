4d6edcdcbaeaccf57e0337e0a049f0c95de23463
Long Hoang <99232763+longhoag@users.noreply.github.com>
2024-03-31 15:33:40 -0700

    update js code

--- server.js
+++ server.js
@@ -4,6 +4,22 @@ const path = require("path");
 const app = express();
 const server = require("http").createServer(app);
 
+const io = require("socket.io")(server);
+
 app.use(express.static(path.join(__dirname + "/site")));
+
+io.on("connection", function(socket){
+    socket.on("newUser", function(username){
+        socket.broadcast.emit("update", username + " has joined the group chat");
+    });
+
+    socket.on("exitUser", function(username) {
+        socket.broadcast.emit("update", username + " has quit the group chat");
+    });
+
+    socket.on("chat", function(message) {
+        socket.broadcast.emit("chat " +  message);
+    });    
+});
 
 server.listen(1000);
