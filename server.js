55dedd6b6539d401170848294c520bfe20c6965e
Long Hoang <99232763+longhoag@users.noreply.github.com>
2024-04-10 16:43:30 -0700

    fix minor details

--- server.js
+++ server.js
@@ -1,25 +1,32 @@
 const express = require("express");
 const path = require("path");
 
+// create node js server
 const app = express();
 const server = require("http").createServer(app);
 
 const io = require("socket.io")(server);
 
+// website source code in folder /site
 app.use(express.static(path.join(__dirname + "/site")));
 
 io.on("connection", function(socket){
+
+    // send signal new users log in
     socket.on("newUser", function(username){
         socket.broadcast.emit("update", username + " has joined the group chat");
     });
 
+    // send signal users exit
     socket.on("exitUser", function(username) {
         socket.broadcast.emit("update", username + " has quit the group chat");
     });
 
+    // send signal chatting
     socket.on("chat", function(message) {
         socket.broadcast.emit("chat", message);
     });    
 });
 
+// server port
 server.listen(8000);
--- site/code.js
+++ site/code.js
@@ -4,10 +4,11 @@
 
     let name;
 
-    // pass messages
+    // click to register new user
     app.querySelector(".join-screen #join-user").addEventListener("click", function(){
         let username = app.querySelector(".join-screen #username").value;
 
+        // if non-input, do nothing
         if (username.length == 0) {
             return;
         }
@@ -28,13 +29,13 @@
         }
     });
 
-    // pass messages
+    // click to pass messages
     app.querySelector(".chat-screen #send-message").addEventListener("click", function() {
         let message = app.querySelector(".chat-screen #message-input").value;
         if (message.length == 0) {
             return;
         }
-        renderMessage("my", {
+        loadMessage("my", {
             username: name,
             text: message
         });
@@ -53,23 +54,25 @@
         }
     });
 
+    // exit chat click button
     app.querySelector(".chat-screen #exit-chat").addEventListener("click", function() {
         socket.emit("exitUser", name);
         window.location.href = window.location.href;
     });
 
-    // render messsages to all users
+    // load messsages to all users
     socket.on("update", function(update) {
-        renderMessage("update", update);
+        loadMessage("update", update);
     });
     socket.on("chat", function(message){
-        renderMessage("other", message);
+        loadMessage("other", message);
     });
 
-
-    function renderMessage(type, message) {
+    // load message by appending html code to show on the screen
+    function loadMessage(type, message) {
         let messageContainer = app.querySelector(".chat-screen .messages");
 
+        // show message me type
         if(type == "my") {
             let element = document.createElement("div");
             element.setAttribute("class", "message my-message");
@@ -81,6 +84,7 @@
             `;
             messageContainer.appendChild(element);
         }
+        // show message others type
         else if (type == "other") {
             let element = document.createElement("div");
             element.setAttribute("class", "message other-message");
@@ -93,6 +97,7 @@
             messageContainer.appendChild(element);
         }
 
+        // update info of users joining and exiting
         else if (type == "update") {
             let element = document.createElement("div");
             element.setAttribute("class", "update");
